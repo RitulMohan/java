98903fc08bc853c480d2d57afcd3eebfe6b464d2
RitulMohan <79750424+RitulMohan@users.noreply.github.com>
2022-02-06 23:58:53 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class loops {
+
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        int n= in.nextInt();
+
+        for (int num=0; num <= n; num++) {
+            System.out.println(num);
+        }
+
+    }
+}
