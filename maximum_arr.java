b5f494c80e3666ee4e45b23cbeea3a3f805b880a
RitulMohan <79750424+RitulMohan@users.noreply.github.com>
2022-02-14 01:02:09 +0530

    Add files via upload

---
+++ maximum_arr.java
@@ -0,0 +1,23 @@
+public class maximum_arr {
+
+    public static void main(String[] args) {
+
+        int[] arr= {1 , 3, 23, 9, 18};
+        System.out.println(max(arr));
+
+    }
+
+    static int max(int[] arr)
+    {
+        int max= arr[0];
+        for (int i = 1; i <arr.length ; i++) {
+            if (arr[i] > max)
+            {
+                max=arr[i];
+        }
+//
+    }
+        return max;
+    }
+
+}
